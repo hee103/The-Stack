1b4c2b6f554d3f105b7261abe3ee1ec04eceef78
hee103 <gmlgml3213@gmail.com>
2025-02-16 20:55:07 +0900

    [Feat] UI 구현
    HomeUI, GameUI, ScoreUI 구현
    [Fix] UIManager, The Stack 수정

--- TheStack/Assets/Scripts/TheStack.cs
+++ TheStack/Assets/Scripts/TheStack.cs
@@ -42,7 +42,7 @@ public class TheStack : MonoBehaviour
     private const string BestScoreKey = "BestScore";
     private const string BestComgoKey = "BestCombo";
 
-    private bool isGameOver = false;
+    private bool isGameOver = true;
 
     // Start is called before the first frame update
     void Start()
@@ -84,11 +84,13 @@ public class TheStack : MonoBehaviour
                 UpdateScore();
                 isGameOver = true;
                 GameOverEffect();
+                UIManager.Instance.SetScoreUI();
             }
         }
 
         MoveBlock();
         transform.position = Vector3.Lerp(transform.position, desiredPosition, StackMovingSpeed * Time.deltaTime);
+
     }
 
     bool Spawn_Block()
@@ -124,6 +126,8 @@ public class TheStack : MonoBehaviour
         lastBlock = newTrans;
 
         isMovingX = !isMovingX;
+
+        UIManager.Instance.UpdateScore();
         return true;
 
     }
@@ -327,5 +331,36 @@ public class TheStack : MonoBehaviour
                 * 100f
             );
         }
+    }
+    public void Restart()
+    {
+        int childCount = transform.childCount;
+
+        for (int i = 0; i < childCount; i++)
+        {
+            Destroy(transform.GetChild(i).gameObject);
+        }
+
+        isGameOver = false;
+
+        lastBlock = null;
+        desiredPosition = Vector3.zero;
+        stackBounds = new Vector3(BoundSize, BoundSize);
+
+        stackcount = -1;
+        isMovingX = true;
+        blockTransition = 0f;
+        secondaryPosition = 0f;
+
+        comboCount = 0;
+        maxCombo = 0;
+
+        prevBlockPosition = Vector3.down;
+
+        prevColor = GetRandomColor();
+        nextColor = GetRandomColor();
+
+        Spawn_Block();
+        Spawn_Block();
     }
 }
